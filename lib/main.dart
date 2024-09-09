8139b9a2abbc94f3dbe274cd6009e804940757ab
JulaSpa <julianspataro@hotmail.com>
2024-09-08 21:47:19 -0300

    the blue box project main route

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:blueboxproject/pages/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Blue Box challenge',
+      initialRoute: '/home',
+      routes: {
+        "/home": (context) => const MyHomePage(),
+      },
+    );
+  }
+}
